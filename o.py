b45211d8e718e6239be8d52577ea37b6891fc1b3
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-08 18:52:18 +0330

    Update o.py

--- o.py
+++ o.py
@@ -10,22 +10,22 @@ def __ip__():
         print(Fore.GREEN + "Helloe . Welcome ;)")
         time.sleep(2)
         try:
-            print(Fore.YELLOW + "\n[!] - Pleass 5 Sec Latter ...")
+            print(Fore.YELLOW + "[!] - Pleass 5 Sec Latter ...")
             time.sleep(1)
-            print(Fore.YELLOW + "\n[!] - Pleass 4 Sec Latter ...")
+            print(Fore.YELLOW + "[!] - Pleass 4 Sec Latter ...")
             time.sleep(1)
-            print(Fore.YELLOW + "\n[!] - Pleass 3 Sec Latter ...")
+            print(Fore.YELLOW + "[!] - Pleass 3 Sec Latter ...")
             time.sleep(1)
-            print(Fore.YELLOW + "\n[!] - Pleass 2 Sec Latter ...")
+            print(Fore.YELLOW + "[!] - Pleass 2 Sec Latter ...")
             time.sleep(1)
-            print(Fore.YELLOW + "\n[!] - Pleass 1 Sec Latter ...")
+            print(Fore.YELLOW + "[!] - Pleass 1 Sec Latter ...")
             time.sleep(1)
         except:
             pass
 
         s1 = socket.gethostname()
         s2 = socket.gethostbyname(s1)
-        print(Fore.GREEN + "\n[+] - Your Ip ==> " + Fore.BLUE + s2)
+        print(Fore,BLUE + "[" + Fore.GREEN = "+"  + Fore.BLUE + "]" + Fore.YELLOW + " - " + Fore.GREEN + "Your Ip " + Fore.BLUE +"==>  " + Fore.YELLOW + s2 )
         time.sleep(5)
     except:
         try:
